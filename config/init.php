dbffddc614565b8fcfd1b3488ae1638b549e09c7
Narutoskee <narutoskee@gmail.com>
2019-03-25 13:34:29 +0300

    добавил еще файлы

--- config/init.php
+++ config/init.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: narutoskee
- * Date: 023 23 03 19
- * Time: 13:14
- */
+define("DEBUG",1); // constant for debug 0 or 1
+define("ROOT",dirname(__DIR__));// constant fot dir path
+define("WWW",ROOT.'/public');//cat public site files
+define("APP",ROOT.'/app');//main cat for mvc
+define("CORE",ROOT.'/vendor/fullstack/core');//cat for core files
+define("LIBS",ROOT.'/vendor/fullstack/core/libs');// cat for libs
+define("CACHE",ROOT.'/tmp/cache');// cat for temp files
+define("CONF",ROOT.'/config');//cat for configuration files
+define("VENDOR",ROOT.'/vendor');//vendor cat path
+define("LAYOUT",'default');
+//http://fullstack.of.by/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+//http://fullstack.of.by/public/i
+$app_path = preg_replace("#[^/]+$#",'',$app_path);
+//http://fullstack.of.by
+$app_path = str_replace('/public/','',$app_path);
+
+define("PATH",$app_path);//const domen path
+define("ADMIN",PATH.'/admin');// admin cat const path
+require_once VENDOR.'/autoload.php';// include composer autoload
--- public/index.php
+++ public/index.php
@@ -1,16 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>Test page</h1>
-    <?php
-    var_dump($_SERVER['QUERY_STRING']);
-    ?>
-</body>
-</html>
+<?php
+require_once dirname(__DIR__).'/config/init.php';
+require_once LIBS .'/functions.php';
+new \fullstack\App();
+
+debug(\fullstack\App::$app->getProperties());
